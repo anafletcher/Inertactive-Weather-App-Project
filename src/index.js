6c07b68a4bead7ac6c05f0829435d26d93ff78ad
anafletcher <153273057+anafletcher@users.noreply.github.com>
2023-12-10 20:41:11 +0000

    API added for search functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+function refreshWeatherData(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityHeaderInput = document.querySelector("#city-header");
+  cityHeaderInput.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function submitCity(event) {
   event.preventDefault();
   let searchFormInput = document.querySelector("#search-form-input");
-  let cityHeaderInput = document.querySelector("#city-header");
-  cityHeaderInput.innerHTML = searchFormInput.value;
+  searchCity(searchFormInput.value);
+}
+
+function searchCity(city) {
+  let apiKey = "f3c5613898a1043cbte4a77d8c1bcfo0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeatherData);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", submitCity);
+
+searchCity("London");
