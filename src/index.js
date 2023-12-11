1a72b596352abdb470fc210de56b0d1cd73073ac
anafletcher <153273057+anafletcher@users.noreply.github.com>
2023-12-11 11:30:54 +0000

    updated icon refresh functionality

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ function refreshWeatherData(response) {
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
+  let iconElement = document.querySelector("#icon");
 
   let date = new Date(response.data.time * 1000);
 
@@ -16,8 +17,7 @@ function refreshWeatherData(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
-
-  console.log(response.data);
+  iconElement.innerHTML = `<img src ="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
 }
 
 function formatDate(date) {
